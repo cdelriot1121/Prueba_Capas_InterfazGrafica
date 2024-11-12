3039424c986f7123d9cc960d2a657e58a98b4750
cdelriot1121 <carandresdelrio1121@gmail.com>
2024-11-11 20:13:18 -0500

    first commit

---
+++ src/logic/LogicMain.java
@@ -0,0 +1,14 @@
+
+package logic;
+import igu.Screen;
+
+public class LogicMain {
+    
+    public static void main(String[] args) {
+        
+        Screen pantalla = new Screen();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+    
+}
